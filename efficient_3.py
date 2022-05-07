46d1c5faa1a7d812aae876fcb5d5e6b6f3d6904b
Michael Defranco <michaeldefranco@usc-guestwireless-upc-newsc2292.usc.edu>
2022-05-07 16:49:59 -0700

    just added new base cases

--- efficient_3.py
+++ efficient_3.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 sys.setrecursionlimit(10**6)
 inputFilename = 'input.txt' #sys.argv[1]
@@ -55,7 +56,7 @@ def findMinCost(s1, s2):
     
     for s1_index in range(1,len(s1)+1):
         for s2_index in range(1, len(s2)+1):
-            matchCost = calculateMatchCost(s1_index, s2_index)
+            matchCost = calculateMatchCost(s1[s1_index-1], s2[s2_index-1])
             
             memo_array[s2_index][s1_index] = min(
                 matchCost + memo_array[s2_index-1][s1_index-1], # match
@@ -64,6 +65,86 @@ def findMinCost(s1, s2):
 
     #memo[(s1_index, s2_index)] = chosenMin
     return memo_array
+
+def find_min_match(x,y):
+    index = 0
+    min_cost = math.inf
+    for i in range(len(y)):
+        cost = calculateMatchCost(x, y[i])
+        if cost < min_cost:
+            index = i
+            min_cost = cost
+    return (min_cost, index)
+
+def find_min_cost_recursive(x, y):
+    if len(x) == 0 and len(y) == 0:
+        return (x, y, 0)
+    elif len(x) == 1 and len(y) > 1:
+        x_str = ''
+        total_cost = 0
+        (min_match_cost, index) = find_min_match(x,y)
+        for i in range(len(y)):
+            if i == index:
+                x_str += x
+            else:
+                x_str = x_str + '_'
+                total_cost += delta
+        return (x_str, y, total_cost + min_match_cost)
+    elif len(y) == 1 and len(x) > 1:
+        y_str = ''
+        total_cost = 0
+        (min_match_cost, index) = find_min_match(y,x)
+        for i in range(len(x)):
+            if i == index:
+                y_str += y
+            else:
+                y_str = y_str + '_'
+                total_cost += delta
+        return (x, y_str, total_cost + min_match_cost)
+    elif len(x) == 1 and len(y) == 1:
+        cost = calculateMatchCost(x,y)
+        return (x,y,cost)
+    elif len(x) == 0:
+        x_str = ''
+        for i in range(len(y)):
+            x_str = x_str + '_'
+        return (x_str, y, (delta * len(y)))
+    elif len(y) == 0:
+        y_str = ''
+        for i in range(len(x)):
+            y_str = y_str + '_'
+        return (x, y_str, (delta * len(x)))
+    
+    x_l, x_r = x[:math.floor(len(x)/2)], x[math.floor(len(x)/2):]
+
+    memo_array_x_l = findMinCost(x_l, y)
+    y_rev = y[::-1]
+    memo_array_x_r = findMinCost(x_r, y_rev)
+    
+    min_opt_val = math.inf
+    for i in range(len(y)+1):
+        opt_val_sum = memo_array_x_l[i][len(x_l)] + memo_array_x_r[len(y)-i][len(x_r)]
+        
+        if opt_val_sum < min_opt_val:
+            min_opt_val = opt_val_sum
+            index_opt = i
+
+    y_l, y_r = y[:index_opt], y[index_opt:]
+
+    left_tuple = find_min_cost_recursive(x_l, y_l)
+    right_tuple = find_min_cost_recursive(x_r, y_r)
+
+    # if last step was a match then add the letters we matched to string
+    if (left_tuple[2] == 0):
+        cost = calculateMatchCost(right_tuple[0], right_tuple[1])
+        right_tuple = (right_tuple[0], right_tuple[1], cost)
+    if (right_tuple[2] == 0):
+        cost = calculateMatchCost(left_tuple[0], left_tuple[1])
+        left_tuple = (left_tuple[0], left_tuple[1], cost)
+
+    return (left_tuple[0] + right_tuple[0], left_tuple[1] + right_tuple[1], left_tuple[2] + right_tuple[2])
+
+
 
 def chooseMin(first, second, third):
     chosen = 0
@@ -77,9 +158,12 @@ def chooseMin(first, second, third):
 
     return chosen
 
-def calculateMatchCost(i, j):
-    char1 = s1[i-1]
-    char2 = s2[j-1]
+def calculateMatchCost(x, y):
+    if len(x) != 1 or len(y) != 1:
+        asdfl = 0
+
+    char1 = x
+    char2 = y
 
     index1 = convertLetterToAlpha(char1)
     index2 = convertLetterToAlpha(char2)
@@ -99,52 +183,13 @@ def convertLetterToAlpha(letter):
     else:
         return -1
 
-def calculate_final_string(memo_array):
-    s1_index = len(s1)
-    s2_index = len(s2)
-    final_s1 = ''
-    final_s2 = ''
-
-    while s1_index > 0 or s2_index > 0:
-        #opt = memo_array[s2_index][s2_index]
-        if s1_index == 0:
-            chose = 1
-        elif s2_index == 0:
-            chose = 2
-        else:
-            chose = chooseMin(memo_array[s2_index-1][s1_index-1], memo_array[s2_index-1][s1_index], memo_array[s2_index][s1_index-1])
-
-        if chose == 0:
-            final_s1 = s1[s1_index-1] + final_s1
-            final_s2 = s2[s2_index-1] + final_s2
-
-            s1_index -= 1
-            s2_index -= 1
-        elif chose == 1:
-            final_s1 = '_' + final_s1
-            final_s2 = s2[s2_index-1] + final_s2
-            s2_index -= 1
-        elif chose == 2:
-            final_s2 = '_' + final_s2
-            final_s1 = s1[s1_index-1] + final_s1
-            s1_index -= 1
-
-    # if len(final_s1) > len(final_s2):
-    #     final_s1 = final_s1[:-1]
-    # else:
-    #     final_s2 = final_s2[:-1]
-
-    return (final_s1, final_s2)
-
 
 def main():
 
-    memo_array = findMinCost()
-    print(memo_array[len(s2)][len(s1)])
-
-    final_strings = calculate_final_string(memo_array)
-    print(final_strings[0])
-    print(final_strings[1])
+    tuple = find_min_cost_recursive(s1, s2)
+    print(tuple[0])
+    print(tuple[1])
+    print(tuple[2])
 
 if __name__ == '__main__':
     main()
